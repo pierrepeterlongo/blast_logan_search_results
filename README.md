8c8655bd0514971b4994123c6394cf02c130f312
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2025-10-16 11:50:07 +0200

    Unify the two scripts

--- README.md
+++ README.md
@@ -5,39 +5,53 @@
 
 Given a query (fasta sequence), and a file containing list of SRA accessions (provided or not by Logan-Search results), run a local blast between the query and the subset of contigs or unitigs containing at least one shared k-mer with the query.
 
+The `logan_blast.sh` script can be used 
+- to directly retrieve the accessions from a Logan-Search session id, create the associated query file, and perform all alignements.
+- or to provide a query file and an accessions file (accessions.txt) directly.
 
-The `blast_logan.sh` script considers a query, and a file continaing list of SRA accessions (in accessions.txt). This list may be provided by a Logan-Search result.
+In any case, for each accession, it will run local blast between the query and the subset of contigs or unitigs containing at least one shared k-mer (k=17 by default) with the query (uses `back_to_sequences`), and save the results in a directory named `<accessions>.recruited_[contigs/unitigs]_vs_query`.
 
-For each accession, it will run local blast between the query and the subset of contigs or unitigs containing at least one shared k-mer (k=17 by default) with the query (uses `back_to_sequences`), and save the results in a directory named `<accessions>.recruited_[contigs/unitigs]_vs_query`.
 
 ## Installation 
-- biopython: `pip3 install biopython`
 - blast: *on mac* `brew install blast` or look at [blast installation web page](https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/)
 - back_to_sequences: see [back_to_sequences installation web page](https://b2s-doc.readthedocs.io/en/latest/usage.html#installation])
+- jq: see [jq installation web page](https://jqlang.org/)
 
-## Running the script
+## Running the `logan_blast.sh` script
 
 ```bash
-sh ./logan_blast.sh 
-
-  -a, --accessions  Path to accessions.txt file. Containing one accession per line)
-  -q, --query       Path to query fasta file
-  -u, --unitigs     Consider the unitigs verison of the accessions instead of contigs
-  -k, --kmer-size K-mer size for sequence recruitment with back_to_sequences (default: 17)
-  -l, --limit     Limit number of accessions to process from accession file (default: no limit)
-  -d, --delete    Delete recruited accessions and accessions files after processing (default: keep all files)
-  -h, --help      Show this help message
+./logan_blast.sh
+  Usage: ./logan_blast.sh --session <logan seesion ID> or (--query <query_file.fa> --accessions <accessions.txt>) [--delete] [--kmer-size <k>] [--limit <n>]
+  Options:
+  Input choice1: session ID
+    -s, --session     Logan session ID, eg. kmviz-b2bce461-ca13-4a45-b0b4-6c894eacf103
+  Input choice2: accessions and query files
+    -a, --accessions  Path to accessions.txt file. Containing one accession per line)
+    -q, --query       Path to query fasta file
+  Global options:
+    -u, --unitigs     Consider the unitigs verison of the accessions instead of contigs
+    -k, --kmer-size K-mer size for sequence recruitment with back_to_sequences (default: 17)
+    -l, --limit     Limit number of accessions to process from accession file (default: no limit)
+    -d, --delete    Delete recruited accessions and accessions files after processing (default: keep all files)
+    -h, --help      Show this help message
 ```
 
-## Creating accessions.txt
+## Example running from session.
+```bash
+bash logan_blast_from_session.sh -s kmviz-b2bce461-ca13-4a45-b0b4-6c894eacf103
+```
+
+## Example running from accessions and query files.
+This usage enables to select specific accessions to process, also ordering them, and to provide a custom query file.
+### Creating accessions.txt
 Logan-Search results can be used to create accessions.txt. Given a logan search result link or after exporting a table from Logan_search interface:
 
 ```bash
 tail -n +2 kmviz-c21feaeb-4f33-4abc-b119-7db7bd47069b/query.tsv | awk '{print $1}' | tr -d "\"" > accessions.txt
 ```
 
-## Example command
+### Running from query and accessions files
 
 ```bash
-sh ./logan_blast.sh  -a example/accessions.txt -q example/query.fa -l 10 -k 27 -u
+sh ./logan_blast.sh  -a example/accessions.txt -q example/query.fa
 ```
--- logan_blast.sh
+++ logan_blast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+SESSION_ID=""
 ACCESSION_FILE=""
 QUERY_FILE=""
 DELETE=false
@@ -8,20 +8,75 @@ UNITIGS=false
 LIMIT=0
 KMER_SIZE=17
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;034m'
+NOCOLOR='\033[0m'
+
 print_help() {
-    echo "Usage: $0 --accessions <accessions.txt> --query <query_file.fa> [--delete] [--kmer-size <k>] [--limit <n>]"
-    echo "Options:"
-    echo "  -a, --accessions  Path to accessions.txt file. Containing one accession per line)"
-    echo "  -q, --query       Path to query fasta file"
-    echo "  -u, --unitigs     Consider the unitigs verison of the accessions instead of contigs"
-    echo "  -k, --kmer-size K-mer size for sequence recruitment with back_to_sequences (default: ${KMER_SIZE})"
-    echo "  -l, --limit     Limit number of accessions to process from accession file (default: no limit)"
-    echo "  -d, --delete    Delete recruited accessions and accessions files after processing (default: keep all files)"
-    echo "  -h, --help      Show this help message"
+    echo -e "Usage: $0 --session <logan seesion ID> or (--query <query_file.fa> --accessions <accessions.txt>) [--delete] [--kmer-size <k>] [--limit <n>]"
+    echo -e "Options:"
+    echo -e " Input choice1: session ID"
+    echo -e "  -s, --session     Logan session ID, eg. kmviz-b2bce461-ca13-4a45-b0b4-6c894eacf103"
+    echo -e " Input choice2: accessions and query files"
+    echo -e "  -a, --accessions  Path to accessions.txt file. Containing one accession per line)"
+    echo -e "  -q, --query       Path to query fasta file"
+    echo -e " Global options:"
+    echo -e "  -u, --unitigs     Consider the unitigs verison of the accessions instead of contigs"
+    echo -e "  -k, --kmer-size K-mer size for sequence recruitment with back_to_sequences (default: ${KMER_SIZE})"
+    echo -e "  -l, --limit     Limit number of accessions to process from accession file (default: no limit)"
+    echo -e "  -d, --delete    Delete recruited accessions and accessions files after processing (default: keep all files)"
+    echo -e "  -h, --help      Show this help message"
+}
+
+run_blast() {
+    set -euo pipefail
+
+    local QUERY_FASTA="$1"
+    local TARGET_FASTA="$2"
+
+
+
+    local QUERY_BASENAME
+    local TARGET_BASENAME
+    QUERY_BASENAME=$(basename "${QUERY_FASTA%.*}")
+    TARGET_BASENAME=$(basename "${TARGET_FASTA%.*}")
+    local OUTPUT_DIR="${QUERY_BASENAME}_vs_${TARGET_BASENAME}"
+
+    mkdir -p "$OUTPUT_DIR"
+
+    echo -e "[INFO] Creating BLAST database from ${TARGET_FASTA}..."
+    makeblastdb -dbtype nucl -in "$TARGET_FASTA" -out "${OUTPUT_DIR}/targets_db" >/dev/null 2>&1
+
+    local QUERY_ID
+    QUERY_ID=$(grep -m1 '^>' "$QUERY_FASTA" | sed 's/^>//;s/ .*//')
+
+    echo -e "[INFO] Running BLAST for each target sequence..."
+
+    grep '^>' "$TARGET_FASTA" | sed 's/^>//;s/ .*//' | while read -r TARGET_ID; do
+        [[ -z "$TARGET_ID" ]] && continue
+        local OUTPUT_FILE="${OUTPUT_DIR}/${QUERY_ID}_vs_${TARGET_ID}.txt"
+
+        echo -e "  - Aligning ${QUERY_ID} vs ${TARGET_ID}..."
+        blastn \
+            -query "$QUERY_FASTA" \
+            -db "${OUTPUT_DIR}/targets_db" \
+            -out "$OUTPUT_FILE" \
+            -outfmt 0 \
+            -num_alignments 1 \
+            -sorthits 0 >/dev/null 2>&1
+    done
+
+    echo -e "\033[92m[INFO] Results are in directory ${OUTPUT_DIR}${NOCOLOR}"
 }
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
+        -s|--session)
+            SESSION_ID="$2"
+            shift 2
+            ;;
         -a|--accessions)
             ACCESSION_FILE="$2"
             shift 2
@@ -51,42 +106,81 @@ while [[ $# -gt 0 ]]; do
             exit 0
             ;;
         *)
-            echo  "\033[0;31mUnknown option: $1\033[0m"
+            echo -e "${RED}Unknown option: $1${NOCOLOR}"
             print_help
             exit 1
             ;;
     esac
 done
 
+# If session option was provided, do not allow accessions or query options
+if [[ -n "$SESSION_ID" && ( -n "$ACCESSION_FILE" || -n "$QUERY_FILE" ) ]]; then
+    echo -e "${RED}Error: --session (-s) cannot be combined with --accessions (-a) or --query (-q).${NOCOLOR}"
+    print_help
+    exit 1
+fi
+
+if [[ -n "$SESSION_ID" ]]; then
+    ACCESSION_FILE="accessions_${SESSION_ID}.txt"
+    if [ ! -f "$ACCESSION_FILE" ]; then
+        wget https://logan-search.org/api/download/${SESSION_ID} -O ${SESSION_ID}.zip
+
+        if [ $? -ne 0 ]; then
+            echo -e "${RED}Error: Failed to download https://logan-search.org/api/download/${SESSION_ID}.${NOCOLOR}"
+            echo -e "${RED}Check your internet connexion. ${NOCOLOR}"
+            exit 1
+        fi
+        unzip ${SESSION_ID}.zip session.json
+        mv session.json ${SESSION_ID}.json
+        jq -r '.. | ._metadata?.ID? // empty | .[]' ${SESSION_ID}.json > ${SESSION_ID}_acc.txt
+        ACCESSION_FILE="${SESSION_ID}_acc.txt"
+        QUERY_FILE="${SESSION_ID}_query.fa"
+        echo -n ">" > ${QUERY_FILE}
+        jq -r '.. | ._query?._name? // empty' ${SESSION_ID}.json >> ${QUERY_FILE}
+        jq -r '.. | ._query?._seq? // empty' ${SESSION_ID}.json >> ${QUERY_FILE}
+    fi
+fi
+
 if [[ -z "$ACCESSION_FILE" || -z "$QUERY_FILE" ]]; then
-    echo  "\033[0;31mError: --accessions and --query are required.\033[0m"
+    echo -e "${RED}Error: --accessions and --query are required.${NOCOLOR}"
     print_help
     exit 1
 fi
 
 if [ ! -f "$QUERY_FILE" ]; then
-    echo  "\033[0;31mError: Query file '$QUERY_FILE' does not exist.\033[0m"
+    echo -e "${RED}Error: Query file '$QUERY_FILE' does not exist.${NOCOLOR}"
     exit 1
 fi
 
 if [ ! -f "$ACCESSION_FILE" ]; then
-    echo  "\033[0;31mError: Accessions file '$ACCESSION_FILE' does not exist.\033[0m"
+    echo -e "${RED}Error: Accessions file '$ACCESSION_FILE' does not exist.${NOCOLOR}"
     exit 1
 fi
 
 if ! [[ "$KMER_SIZE" =~ ^[0-9]+$ ]] || [ "$KMER_SIZE" -le 0 ]; then
-    echo  "\033[0;31mError: K-mer size must be a positive integer.\033[0m"
+    echo -e "${RED}Error: K-mer size must be a positive integer.${NOCOLOR}"
     exit 1
 fi
 
 if ! [[ "$LIMIT" =~ ^[0-9]+$ ]] || [ "$LIMIT" -lt 0 ]; then
-    echo  "\033[0;31mError: Limit must be a non-negative integer.\033[0m"
+    echo -e "${RED}Error: Limit must be a non-negative integer.${NOCOLOR}"
     exit 1
 fi
 
 ## check that back_to_sequences is installed
 if ! command -v back_to_sequences &> /dev/null; then
-    echo  "\033[0;31mError: back_to_sequences could not be found. Please install it first https://github.com/pierrepeterlongo/back_to_sequences.\033[0m"
+    echo -e "${RED}Error: back_to_sequences could not be found. Please install it first https://github.com/pierrepeterlongo/back_to_sequences.${NOCOLOR}"
+    exit 1
+fi
+
+if ! command -v makeblastdb >/dev/null 2>&1 || ! command -v blastn >/dev/null 2>&1; then
+    echo -e "${RED}Error: BLAST+ executables 'makeblastdb' and/or 'blastn' not found in PATH.${NOCOLOR}"
+    return 1
+fi
+
+# check that jq is installed
+if ! command -v jq >/dev/null 2>&1; then
+    echo -e "${RED}Error: 'jq' could not be found. Please install jq (https://jqlang.org/) and ensure it's in your PATH.${NOCOLOR}"
     exit 1
 fi
 
@@ -104,51 +198,50 @@ QUERY_BASENAME="${QUERY_BASENAME%%.*}"
 counter=0
 while read accession; do
     if [ "$LIMIT" -ne 0 ] && [ "$counter" -ge "$LIMIT" ]; then
-        echo  "\n\033[0;33mReached limit of $LIMIT accessions. Stopping further processing.\033[0m"
+        echo -e "\n${YELLOW}Reached limit of $LIMIT accessions. Stopping further processing.${NOCOLOR}"
         break
     fi
     counter=$((counter + 1))
-    echo  "\n\033[1;34m========================================\033[0m"
-    echo  "\033[1;36m>>> Processing accession: ${accession} <<<\033[0m"
-    echo  "\033[1;34m========================================\033[0m"
+    echo -e "\n\033[1;34m========================================${NOCOLOR}"
+    echo -e "\033[1;36m>>> Processing accession: ${accession} <<<${NOCOLOR}"
+    echo -e "\033[1;34m========================================${NOCOLOR}"
 	if [ ! -f "${accession}.${type}s.fa.zst" ]; then
-		echo "\033[0;33mDownloading ${accession}.${type}s.fa.zst...\033[0m"
+		echo -e "${YELLOW}Downloading ${accession}.${type}s.fa.zst...${NOCOLOR}"
         if [ "$UNITIGS" = false ]; then
-		    aws s3 cp s3://logan-pub/c/${accession}/${accession}.contigs.fa.zst . --no-sign-request
+            wget https://s3.amazonaws.com/logan-pub/c/${accession}/${accession}.contigs.fa.zst 
             else 
-		    aws s3 cp s3://logan-pub/u/${accession}/${accession}.unitigs.fa.zst . --no-sign-request
+            wget https://s3.amazonaws.com/logan-pub/u/${accession}/${accession}.unitigs.fa.zst
         fi
 
         if [ $? -ne 0 ]; then
-            echo "\033[0;31mError: Failed to download ${accession}.${type}s.fa.zst from S3. Skipping this accession.\033[0m"
+            echo -e "${RED}Error: Failed to download ${accession}.${type}s.fa.zst from S3. Skipping this accession.${NOCOLOR}"
             if [ "$UNITIGS" = false ]; then
-                echo "\033[0;31mThis usually occurs because some logan accessions do not have contigs files (they have only unitigs files).\033[0m"
+                echo -e "${RED}This usually occurs because some logan accessions do not have contigs files (they have only unitigs files).${NOCOLOR}"
             fi
             continue
         fi
 	else
-		echo "\033[0;33mUsing existing local version of ${accession}.${type}s.fa.zst...\033[0m"
+		echo -e "${YELLOW}Using existing local version of ${accession}.${type}s.fa.zst...${NOCOLOR}"
 	fi
-	echo "\033[0;33mRecruiting sequences from ${accession}.${type}s.fa.zst with a match with ${QUERY_FILE}...\033[0m"
-	echo "\033[0;32mback_to_sequences --kmer-size ${KMER_SIZE} --in-kmers ${QUERY_FILE} --in-sequences ${accession}.${type}s.fa.zst --out-sequences T.fa\033[0m"
+	echo -e "${YELLOW}Recruiting sequences from ${accession}.${type}s.fa.zst with a match with ${QUERY_FILE}...${NOCOLOR}"
+	echo -e "${GREEN}back_to_sequences --kmer-size ${KMER_SIZE} --in-kmers ${QUERY_FILE} --in-sequences ${accession}.${type}s.fa.zst --out-sequences T.fa${NOCOLOR}"
 	back_to_sequences --kmer-size ${KMER_SIZE} --in-kmers ${QUERY_FILE} --in-sequences  ${accession}.${type}s.fa.zst --out-sequences ${accession}.recruited_${type}s.fa > /dev/null 2>&1
 
     ## check if any sequences were recruited
     if [ ! -s "${accession}.recruited_${type}s.fa" ]; then
-        echo "\033[0;33m\tNo sequences were recruited from ${accession}.${type}s.fa.zst. Skipping BLAST step.\033[0m"
+        echo -e "${YELLOW}\tNo sequences were recruited from ${accession}.${type}s.fa.zst. Skipping BLAST step.${NOCOLOR}"
         if [ "$DELETE" = true ]; then
-            echo "\033[0;33mDeleting ${accession}.recruited_${type}s.fa and ${accession}.${type}s.fa.zst...\033[0m"
+            echo -e "${YELLOW}Deleting ${accession}.recruited_${type}s.fa and ${accession}.${type}s.fa.zst...${NOCOLOR}"
             rm -f ${accession}.recruited_${type}s.fa
             rm -f ${accession}.${type}s.fa.zst
         fi
         continue
     fi
 
-	echo "\033[0;33mAligning recruited sequences from ${accession}.${type}s.fa.zst with ${QUERY_FILE}...\033[0m"
-	echo "\033[0;32mpython3 local_blast.py --target ${QUERY_FILE} --query ${accession}.recruited_${type}s.fa \033[0m"
-	python3 local_blast.py --target ${QUERY_FILE} --query ${accession}.recruited_${type}s.fa
+	echo -e "${YELLOW}Aligning recruited sequences from ${accession}.${type}s.fa.zst with ${QUERY_FILE}...${NOCOLOR}"
+    run_blast "$QUERY_FILE" "${accession}.recruited_${type}s.fa"
     if [ "$DELETE" = true ]; then
-        echo "\033[0;33mDeleting ${accession}.recruited_${type}s.fa and ${accession}.${type}s.fa.zst...\033[0m"
+        echo -e "${YELLOW}Deleting ${accession}.recruited_${type}s.fa and ${accession}.${type}s.fa.zst...${NOCOLOR}"
         rm -f ${accession}.recruited_${type}s.fa
         rm -f ${accession}.${type}s.fa.zst
     fi
@@ -158,14 +251,14 @@ done < ${ACCESSION_FILE}
 
 echo
 
-    echo  "\n\033[1;34m================\033[0m"
-    echo  "\033[1;36m>>> All done <<<\033[0m"
-    echo  "\033[1;34m================\033[0m\n"
+    echo -e "\n${BLUE}================"
+    echo -e "${BLUE}>>> All done <<<"
+    echo -e "${BLUE}================\n"
 # if --delete was not used, show user how to remove all intermediate files
 if [ "$DELETE" = false ]; then
-    echo "\033[0;33mYou  did not use --delete option. So you can manually remove all intermediate files (recruited ${type}s and ${type}s files) by running:\033[0m"
-    echo "\033[0;32mrm -f *.recruited_${type}s.fa *.${type}s.fa.zst\033[0m"
+    echo -e "${YELLOW}You  did not use --delete option. So you can manually remove all intermediate files (recruited ${type}s and ${type}s files) by running:${NOCOLOR}"
+    echo -e "${GREEN}rm -f *.recruited_${type}s.fa *.${type}s.fa.zst${NOCOLOR}"
 fi
 
 echo
-echo "\033[0;33mResults can be found in directories <accessions>.recruited_${type}s_vs_${QUERY_BASENAME} for each accession id in the ${ACCESSION_FILE}.\033[0m"
+echo -e "${YELLOW}Results can be found in directories <accessions>.recruited_${type}s_vs_${QUERY_BASENAME} for each accession id in the ${ACCESSION_FILE}.${NOCOLOR}"
